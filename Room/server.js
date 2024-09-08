436b9d78df43925ef3fb3b36cb864cad120c8842
mvtvn78 <mvtvn78@gmail.com>
2024-09-08 22:03:59 +0700

    Day 2

--- Room/server.js
+++ Room/server.js
@@ -6,34 +6,21 @@ app.set("views","./views")
 var server = require("http").Server(app);
 var io = require("socket.io")(server)
 server.listen(3000)
-var mangUsers=[]
 io.on("connection",(socket)=>{
     console.log("There person connect "+ socket.id);
-    socket.on("client-send-Username",(data)=>{
-        console.log(socket.id+" send data "+ data);
-        if(mangUsers.indexOf(data) >=0){
-            socket.emit("Server-send-failed");
-        }
-
-        else
-        {
-            mangUsers.push(data)
-            socket.Username=data;
-            socket.emit("Server-send-success",data);
-            io.sockets.emit("Server-send-Users",mangUsers)
-        }
+    
+    socket.on("make-room",(data)=>{
+        socket.join(data);
+        socket.roomCurr = data;
+        var mangRooms=[]
+        for (const x of socket.adapter.rooms) {
+            mangRooms.push(x);
+          }
+        io.sockets.emit("server-send-rooms",mangRooms)
+        socket.emit("server-send-room-socket",socket.roomCurr);
     })
-    socket.on("client-logout-Username",()=>{
-        mangUsers.splice(
-            mangUsers.indexOf(socket.Username),1
-        )
-        socket.broadcast.emit("Server-send-Users",mangUsers)
-    })
-    socket.on("user-send-msg",(data)=>{
-        io.sockets.emit("server-send-msg",{
-            username: socket.Username,
-            msgText : data
-        })
+    socket.on("user-chat",(data)=>{
+        io.sockets.in(socket.roomCurr).emit("server-chat",data)
     })
     socket.on("disconnect",()=>{
         console.log(socket.id + " Disconnect !!")
